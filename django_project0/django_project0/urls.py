4ff27c446ad51c0449a0650f24f4f59cc72f41ed
Asif <ama85bd@gmail.com>
2020-04-27 23:30:59 +0600

    final create app

--- django_project0/django_project0/urls.py
+++ django_project0/django_project0/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
+from my_app0 import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
+    url(r'^my_app0/(\d+)/', views.pet_detail, name='pet_detail'),
+
 ]
